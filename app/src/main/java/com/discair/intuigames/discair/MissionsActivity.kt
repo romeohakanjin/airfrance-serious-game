4cf115633418ab2644c447dab7998f1d1f58a75c
Bioski1 <bioski10@gmail.com>
2019-03-18 22:36:35 +0100

    Nouvelle vue

---
+++ app/src/main/java/com/discair/intuigames/discair/MissionsActivity.kt
@@ -0,0 +1,12 @@
+package com.discair.intuigames.discair
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MissionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_missions)
+    }
+}
